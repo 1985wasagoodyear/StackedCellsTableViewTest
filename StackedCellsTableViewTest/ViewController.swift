8ec1864b33e4f8ca02bdef638cae41c68f5d07e3
1985wasagoodyear <1985wasagoodyear>
2019-08-12 17:09:31 -0400

    Initial Commit

---
+++ StackedCellsTableViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackedCellsTableViewTest
+//
+//  Created by K Y on 8/12/19.
+//  Copyright © 2019 K Y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
